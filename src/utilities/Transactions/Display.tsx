255f4e519c5dc03d1becc5c103853127477fb4c1
The24Kings <24kingsunite@gmail.com>
2025-03-06 11:20:33 -0800

    more data in accordion

--- src/utilities/Transactions/Display.tsx
+++ src/utilities/Transactions/Display.tsx
@@ -1,62 +1,77 @@
 import React, { useState } from "react";
 import {
-    IonItem,
-    IonLabel,
-    IonNote,
-    IonItemGroup,
-    IonGrid,
-    IonCol,
-    IonItemDivider,
-    IonRow,
+	IonCol,
+	IonGrid,
+	IonIcon,
+	IonItem,
+	IonItemDivider,
+	IonItemGroup,
+	IonLabel,
+	IonNote,
+	IonRow,
+	IonTextarea
 } from "@ionic/react";
-
 import Transaction from "./Transaction";
+import { chevronDownOutline, chevronForwardOutline, chevronUpOutline } from "ionicons/icons";
 
 interface DisplayTransactionsProps {
-    transactions: Transaction[];
+	transactions: Transaction[];
 }
 
 const DisplayTransactions: React.FC<DisplayTransactionsProps> = ({ transactions }) => {
-    const [expandedTransactionId, setExpandedTransactionId] = useState<string | null>(null);
+	const [expandedTransactionId, setExpandedTransactionId] = useState<string | null>(null);
 
-    // Group the transactions by month
-    const groups = transactions.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
-        .reduce((groups, transaction) => {
-            const month = new Date(transaction.date).toLocaleString('default', { month: 'long', year: 'numeric' });
+	// Group the transactions by month
+	const groups = transactions
+		.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+		.reduce(
+			(groups, transaction) => {
+				const month = new Date(transaction.date).toLocaleString("default", {
+					month: "long",
+					year: "numeric"
+				});
 
-            if (!groups[month]) {
-                groups[month] = [];
-            }
+				if (!groups[month]) {
+					groups[month] = [];
+				}
 
-            groups[month].push(transaction);
+				groups[month].push(transaction);
 
-            return groups;
-        }, {} as { [key: string]: Transaction[] })
+				return groups;
+			},
+			{} as { [key: string]: Transaction[] }
+		);
 
-    const toggleAccordion = (transactionId: string) => {
-        if (expandedTransactionId === transactionId) {
-            setExpandedTransactionId(null);
-        } else {
-            setExpandedTransactionId(transactionId);
-        }
-    }
-    
-    return (
-        <React.Fragment>
-            <div className="transactions">
-                <div hidden={!(transactions.length === 0)}><h1>Loading...</h1></div>
+	const toggleAccordion = (transactionId: string) => {
+		if (expandedTransactionId === transactionId) {
+			setExpandedTransactionId(null);
+		} else {
+			setExpandedTransactionId(transactionId);
+		}
+	};
 
-                {/* Group the transactions by month */}
-                {Object.keys(groups).map((month) => (
-                    <IonItemGroup key={month}>
-                        <IonItemDivider>
-                            <IonLabel>{month}</IonLabel>
-                        </IonItemDivider>
+	return (
+		<React.Fragment>
+			<div className="transactions">
+				<div hidden={!(transactions.length === 0)}>
+					<h1>Loading...</h1>
+				</div>
 
-                        {/* Display the transactions */}
-                        {groups[month].map((transaction) => (
-                            <div key={transaction.id}>
-                                <IonItem button detail={true} onClick={() => toggleAccordion(transaction.id)}>
+				{/* Group the transactions by month */}
+				{Object.keys(groups).map((month) => (
+					<IonItemGroup key={month}>
+						<IonItemDivider>
+							<IonLabel>{month}</IonLabel>
+						</IonItemDivider>
+
+						{/* Display the transactions */}
+						{groups[month].map((transaction) => (
+							<div key={transaction.id}>
+                                <IonItem
+                                    button
+                                    detail={false}
+                                    onClick={() => toggleAccordion(transaction.id)}
+                                >
                                     <IonLabel>
                                         <IonNote>
                                             {transaction.category} - {transaction.subCategory}
@@ -67,24 +82,54 @@ const DisplayTransactions: React.FC<DisplayTransactionsProps> = ({ transactions 
                                                     <h2>{transaction.title}</h2>
                                                 </IonCol>
                                                 <IonCol className="ion-text-right">
-                                                    {transaction.type === "Income" ? "+" : "-"} ${transaction.amount}
+                                                    {transaction.type === "Income" ? "+" : "-"} $
+                                                    {transaction.amount}
                                                 </IonCol>
                                             </IonRow>
                                         </IonGrid>
                                     </IonLabel>
+                                    {expandedTransactionId === transaction.id ? (
+                                        <IonIcon size="small" icon={chevronDownOutline} />
+                                    ) : (
+                                        <IonIcon size="small" icon={chevronForwardOutline} />
+                                    )}
                                 </IonItem>
-                                {expandedTransactionId === transaction.id && (
-                                    <div className="accordion-content">
-                                        <p>Additional details about the transaction...</p>
-                                    </div>
-                                )}
-                            </div>
-                        ))}
-                    </IonItemGroup>
-                ))}
-            </div>
-        </React.Fragment>
-    );
+								{expandedTransactionId === transaction.id && (
+									<div className="accordion-content">
+										<IonItem color="light">
+											<IonGrid fixed={true} className="ion-no-padding">
+												<IonRow>
+													<IonCol className="ion-padding-vertical">
+														{new Date(
+															transaction.date
+														).toLocaleDateString()}
+													</IonCol>
+												</IonRow>
+                                                <IonRow>
+                                                    <IonTextarea
+                                                        className="custom ion-no-padding"
+                                                        shape="round"
+                                                        autoGrow={true}
+                                                        readonly={true}
+                                                        value={transaction.description || "No description available."}
+                                                    />
+                                                </IonRow>
+												<IonRow>
+													<IonCol className="ion-text-right ion-padding-vertical">
+														<IonNote>{transaction.id}</IonNote>
+													</IonCol>
+												</IonRow>
+											</IonGrid>
+										</IonItem>
+									</div>
+								)}
+							</div>
+						))}
+					</IonItemGroup>
+				))}
+			</div>
+		</React.Fragment>
+	);
 };
 
 export default DisplayTransactions;
