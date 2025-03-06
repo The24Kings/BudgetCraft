728687db5ae3e42b4e68a19fd3ef3d17b9dc3423
The24Kings <24kingsunite@gmail.com>
2025-03-05 18:07:21 -0800

    default values

--- src/utilities/Transactions/Transaction.tsx
+++ src/utilities/Transactions/Transaction.tsx
@@ -1,13 +1,13 @@
 class Transaction {
     constructor(
-        public id: string,
-        public type: string,
-        public category: string,
-        public subCategory: string,
-        public title: string,
-        public date: string,
-        public description: string,
-        public amount: number
+        public id: string = "",
+        public type: string = "",
+        public category: string = "",
+        public subCategory: string = "",
+        public title: string = "",
+        public date: string = "",
+        public description: string = "",
+        public amount: number = 0
     ) {}
 };
 
