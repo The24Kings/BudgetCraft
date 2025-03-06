57086f01310778026bc0e9638dc2253f0204ff02
The24Kings <24kingsunite@gmail.com>
2025-03-05 18:07:45 -0800

    added components to landing page

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,23 +1,70 @@
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { firestore } from "../utilities/FirebaseConfig";
+
+import Transaction from "../utilities/Transactions/Transaction";
+import DisplayTransactions from "../utilities/Transactions/Display";
+import { Category, parseJSON } from "../utilities/Categories";
+
+import jsonData from "../categories.json";
+
 import "./Container.css";
+import AddTransactions from "../utilities/Transactions/Add";
 
 interface ContainerProps {
-	name: string;
+    userID: string;
 }
 
-const Container: React.FC<ContainerProps> = ({ name }) => {
+const Container: React.FC<ContainerProps> = ({ userID }) => {
+    let [categoryData, setCategoryData] = useState<Category[]>([]);
+    let [transactionData, setTransactionData] = useState<Transaction[]>([]);
+
+    // Load the transactions from Firebase
+    useEffect(() => {
+        const fetchTransactions = async () => {
+            try {
+                //await getDocuments(`users/${userID}/transactions`);
+                const querySnapshot = await getDocs(collection(firestore, `users/${userID}/transactions`));
+
+                console.log("Fetching transactions...");
+
+                // Parse the documents into Transaction objects
+                const transactionData = querySnapshot.docs.map((doc) => {
+                    const data = doc.data();
+
+                    return new Transaction(data.id, data.type, data.category, data.subCategory, data.title, data.date, data.description, data.amount);
+                });
+
+                setTransactionData(transactionData);
+            } catch (error) {
+                console.error("Failed to fetch transactions...");
+            }
+        };
+
+        const interval = setInterval(() => {
+            fetchTransactions();
+        }, 1000);
+
+        return () => clearInterval(interval);
+
+    }, []);
+
+    // Load the JSON data
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCategoryData(parseJSON(jsonData));
+        }, 100);
+
+        return () => clearInterval(interval);
+    }, []);
+    
 	return (
 		<div className="container">
-			<strong>{name}</strong>
-			<p>
-				Explore{" "}
-				<a
-					target="_blank"
-					rel="noopener noreferrer"
-					href="https://ionicframework.com/docs/components"
-				>
-					UI Components
-				</a>
-			</p>
+            {/* Display the transactions */}
+            <DisplayTransactions transactions={transactionData} />
+
+            {/* Add Transactions */}
+            <AddTransactions categories={categoryData} userID={userID} />
 		</div>
 	);
 };
