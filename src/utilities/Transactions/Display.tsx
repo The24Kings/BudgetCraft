96dedd7f6d18d02e20f56339fe31b9e86e3b0aa6
The24Kings <24kingsunite@gmail.com>
2025-03-05 19:17:58 -0800

    more layout designs

--- src/utilities/Transactions/Display.tsx
+++ src/utilities/Transactions/Display.tsx
@@ -7,34 +7,69 @@ import {
     IonNote,
     IonContent,
     IonItemGroup,
+    IonGrid,
+    IonCol,
+    IonItemDivider,
+    IonRow,
+    IonIcon,
 } from "@ionic/react";
 
 import Transaction from "./Transaction";
+import { addCircle, removeCircle } from "ionicons/icons";
 
 interface DisplayTransactionsProps {
     transactions: Transaction[];
 }
 
 const DisplayTransactions: React.FC<DisplayTransactionsProps> = ({ transactions }) => {
+    // Group the transactions by month
+    const groups = transactions.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+        .reduce((groups, transaction) => {
+            const month = new Date(transaction.date).toLocaleString('default', { month: 'long', year: 'numeric' });
+
+            if (!groups[month]) {
+                groups[month] = [];
+            }
+
+            groups[month].push(transaction);
+
+            return groups;
+        }, {} as { [key: string]: Transaction[] })
+    
     return (
         <React.Fragment>
-            <div className="container">
+            <div className="transactions">
                 <div hidden={!(transactions.length === 0)}><h1>Loading...</h1></div>
 
                 {/* TODO: Group by month */}
-                <IonItemGroup>
-                    {transactions.map((transaction) => (
-                        <div className="ion-padding-top" key={transaction.id}>
-                            <IonItem button={true} detail={true}>
-                                <IonLabel>
-                                    <h2>{transaction.title}</h2>
-                                    <p>{transaction.amount}</p>
-                                    <IonNote>{new Date(transaction.date).toLocaleDateString()}</IonNote>
-                                </IonLabel>
-                            </IonItem>
-                        </div>
-                    ))}
-                </IonItemGroup>
+                {Object.keys(groups).map((month) => (
+                    <IonItemGroup key={month}>
+                        <IonItemDivider>
+                            <IonLabel>{month}</IonLabel>
+                        </IonItemDivider>
+                        {groups[month].map((transaction) => (
+                            <div key={transaction.id}>
+                                <IonItem button detail={true}>
+                                    <IonLabel>
+                                        <IonNote>
+                                            {transaction.category} - {transaction.subCategory}
+                                        </IonNote>
+                                        <IonGrid fixed={true} className="ion-no-padding">
+                                            <IonRow className="ion-text-left ion-padding-top">
+                                                <IonCol>
+                                                    <h2>{transaction.title}</h2>
+                                                </IonCol>
+                                                <IonCol className="ion-text-right">
+                                                    {transaction.type === "Income" ? "+" : "-"} ${transaction.amount}
+                                                </IonCol>
+                                            </IonRow>
+                                        </IonGrid>
+                                    </IonLabel>
+                                </IonItem>
+                            </div>
+                        ))}
+                    </IonItemGroup>
+                ))}
             </div>
         </React.Fragment>
     );
