6cff70d7cd91207b3666646fcafb1454478050e4
The24Kings <24kingsunite@gmail.com>
2025-03-07 15:01:46 -0800

    refactor fetch scheme

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, limit, QuerySnapshot, DocumentData, orderBy } from "firebase/firestore";
 import jsonData from "../categories.json";
 import { Category, parseJSON } from "../utilities/Categories";
 import { firestore } from "../utilities/FirebaseConfig";
@@ -19,34 +19,37 @@ const Container: React.FC<ContainerProps> = ({ userID }) => {
 	// Load the transactions from Firebase
 	useEffect(() => {
 		const fetchTransactions = async () => {
+			let querySnapshot: QuerySnapshot<DocumentData>;
+
 			try {
-				//await getDocuments(`users/${userID}/transactions`);
-				const querySnapshot = await getDocs(
-					collection(firestore, `users/${userID}/transactions`)
-				);
+                console.log("Fetching transactions...");
 
-				console.log("Fetching transactions...");
-
-				// Parse the documents into Transaction objects
-				const transactionData = querySnapshot.docs.map((doc) => {
-					const data = doc.data();
-
-					return new Transaction(
-						doc.id,
-						data.type,
-						data.category,
-						data.subCategory,
-						data.title,
-						data.date,
-						data.description,
-						data.amount
-					);
-				});
-
-				setTransactionData(transactionData);
+                querySnapshot = await getDocs(
+					query(collection(firestore, `users/${userID}/transactions`), orderBy("date"), limit(10))
+                );
 			} catch (error) {
 				console.error("Failed to fetch transactions...");
-			}
+			} finally {
+                console.log("Transactions fetched successfully:", querySnapshot.docs.length);
+
+                // Parse the documents into Transaction objects
+                const transactionData = querySnapshot.docs.map((doc) => {
+                    const data = doc.data();
+
+                    return new Transaction(
+                        doc.id,
+                        data.type,
+                        data.category,
+                        data.subCategory,
+                        data.title,
+                        data.date,
+                        data.description,
+                        data.amount
+                    );
+                });
+
+                setTransactionData(transactionData);
+            }
 		};
 
 		const interval = setInterval(() => {
