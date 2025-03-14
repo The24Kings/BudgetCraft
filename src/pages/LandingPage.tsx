92ec7832b55253bc321894af38b091a8cf0e504e
Cole Devorak <csdevorak1435@students.lcstate.edu>
2025-03-14 18:21:02 +0000

    Merged PR 25: User login page with email and password
    This now allows the user to sign in with email and password instead of Google pop up. It also creates a document for the user in Firebase that contains the user's name, email, creation date, and last sign in. It also sends the categories json so it can later be edited by the user adding their own custom sub-categories.
    
    Related work items: #85

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,107 +1,268 @@
-import React, { useState } from "react";
-import {    
-    IonButton,
-    IonButtons,
-    IonContent,
-    IonHeader,
-    IonImg,
-    IonMenu,
-    IonMenuButton,
-    IonMenuToggle,
-    IonPage,
-    IonText,
-    IonTitle,
-    IonToolbar
+import React, { useEffect, useState } from "react";
+import {
+	createUserWithEmailAndPassword,
+	onAuthStateChanged,
+	signInWithEmailAndPassword,
+	signOut
+} from "firebase/auth";
+import {
+	collection,
+	doc,
+	getDoc,
+	serverTimestamp,
+	setDoc,
+	Timestamp,
+	updateDoc
+} from "firebase/firestore";
+import {
+	IonButton,
+	IonButtons,
+	IonContent,
+	IonHeader,
+	IonImg,
+	IonInput,
+	IonItem,
+	IonLabel,
+	IonMenu,
+	IonMenuButton,
+	IonMenuToggle,
+	IonPage,
+	IonText,
+	IonTitle,
+	IonToolbar
 } from "@ionic/react";
+import categoriesData from "../categories.json";
 import Container from "../components/Container";
-import { logout, signInWithGoogle } from "../utilities/AuthService";
-import "./LandingPage.css";
+import { auth, firestore } from "../utilities/FirebaseConfig";
 
 const LandingPage: React.FC = () => {
-    const [user, setUser] = useState<any>(null);
+	const [user, setUser] = useState<any>(null);
+	const [email, setEmail] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+	const [firstName, setFirstName] = useState<string>("");
+	const [lastName, setLastName] = useState<string>("");
+	const [isRegistering, setIsRegistering] = useState<boolean>(false);
+	const [errorMessage, setErrorMessage] = useState<string>("");
+	const [userData, setUserData] = useState<any>(null);
 
-    //TODO: Look into cookies to make login persistant between sessions
-    const handleSignIn = async () => {
-        try {
-            const userData = await signInWithGoogle();
-            setUser(userData);
-        } catch (error) {
-            console.error("Login failed", error);
-        }
-    };
+	// Handle user registration
+	const handleRegister = async () => {
+		try {
+			const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+			const user = userCredential.user;
 
-    const handleLogout = async () => {
-        try {
-            await logout();
-            setUser(null);
-        } catch (error) {
-            console.error("Logout failed", error);
-        }
-    };
+			// Create a document in Firestore for the new user
+			const userRef = doc(collection(firestore, "users"), user.uid);
+			await setDoc(userRef, {
+				email: email,
+				displayName: `${firstName} ${lastName}`,
+				createdAt: Timestamp.now(),
+				lastSignIn: null
+			});
 
-    return (
-        <React.Fragment>
-            {/* Side Menu */}
-            <IonMenu contentId="main-content">
-                <IonHeader>
-                    <IonToolbar>
-                        <IonTitle>User Settings</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                {user && (
-                    <IonContent className="ion-padding">
-                            <IonText className="center-text">
-                                <h2>Welcome, {user.displayName}!</h2>
-                            </IonText>
-                            <IonImg src={user.photoURL} alt="User Avatar" className="user-avatar" /> {/*TODO: Make the image a cirlce with CSS rather than the square*/}
-                            <IonButton expand="full" color="danger" onClick={handleLogout}>
-                                Logout
-                            </IonButton>
-                    </IonContent>
-                )}
-            </IonMenu>
+			// Create a "Settings" collection and "Categories" document for the user
+			const settingsRef = doc(
+				collection(firestore, `users/${user.uid}/settings`),
+				"categories"
+			);
+			await setDoc(settingsRef, {
+                categories: categoriesData,
+                lastUpdated: Timestamp.now()
+            }); // Fill with data from categories.json
 
-            {/* Main Page Content */}
-            <IonPage id="main-content">
-                <IonHeader>
-                    <IonToolbar>
-                        <IonTitle>Landing Page</IonTitle>
-                        <IonButtons slot="end">
-                            {user ? (
-                                // Use a button that calls IonMenuButton functionality
-                                <IonMenuToggle id="profile-menu-button">
-                                    <IonImg
-                                        src={user.photoURL} //TODO: Find a way to cache the image so it doesn't have to be downloaded every time
-                                        alt="User Profile"
-                                        className="menu-avatar"
-                                    />
-                                </IonMenuToggle>
-                            ) : (
-                                <IonMenuButton />
-                            )}
-                        </IonButtons>
-                    </IonToolbar>
-                </IonHeader>
-                <IonContent fullscreen>
-                    {!user ? (
-                        <div className="login-container">
-                            <div className="login-text">
-                                <IonText>
-                                    <h2>Welcome to BudgetCraft!</h2>
-                                    <p>Please sign in to continue</p>
-                                </IonText>
-                            </div>
-                            <IonButton expand="full" onClick={handleSignIn}>
-                                Sign in with Google
-                            </IonButton>
-                        </div>
-                    ) : (
-                        <Container userID="test-user"/>
-                    )}
-                </IonContent>
-            </IonPage>
-        </React.Fragment>
-    );
-}
+			console.log("User  registered successfully and documents created");
+
+			// Clear input fields
+			setEmail("");
+			setPassword("");
+			setFirstName("");
+			setLastName("");
+		} catch (error) {
+			console.error("Registration Error:", error);
+			setErrorMessage(error.message);
+		}
+	};
+
+	// Handle user sign in
+	const handleSignIn = async () => {
+		try {
+			const userCredential = await signInWithEmailAndPassword(auth, email, password);
+			const user = userCredential.user;
+
+			// Fetch user data from Firestore
+			const userRef = doc(firestore, "users", user.uid);
+			const userDoc = await getDoc(userRef);
+			if (userDoc.exists()) {
+				const userData = userDoc.data();
+				setUserData(userData);
+
+				await updateDoc(userRef, {
+					lastSignIn: serverTimestamp() // Update last sign-in time
+				});
+			} else {
+				console.log("No such document!");
+			}
+
+			console.log("User  signed in successfully");
+			setUser(user);
+			setErrorMessage("");
+
+			setEmail("");
+			setPassword("");
+		} catch (error) {
+			console.error("Sign in Error:", error);
+			setErrorMessage(error.message);
+		}
+	};
+
+	// Handle user logout
+	const handleLogout = async () => {
+		await signOut(auth)
+			.finally(() => {
+				console.log("User  signed out");
+				setUser(null);
+				setUserData(null);
+				setFirstName("");
+				setLastName("");
+			})
+			.catch((error) => {
+				console.error("Logout Error:", error);
+			});
+	};
+
+	// Check if user is signed in
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, (user) => {
+			if (user) {
+				console.log("User  signed in using Auth Change Listener");
+				setUser(user);
+				// Fetch user data from Firestore
+				const fetchUserData = async (uid: string) => {
+					const userRef = doc(firestore, "users", uid);
+					const userDoc = await getDoc(userRef);
+					if (userDoc.exists()) {
+						const userData = userDoc.data();
+						setUserData(userData);
+					} else {
+						console.log("No such document!");
+					}
+				};
+				fetchUserData(user.uid);
+			} else {
+				console.log("No user signed in");
+				setUser(null);
+				setUserData(null);
+				setFirstName("");
+				setLastName("");
+			}
+		});
+		return () => unsubscribe();
+	}, []);
+
+	return (
+		<React.Fragment>
+			<IonMenu contentId="main-content">
+				<IonHeader>
+					<IonToolbar>
+						<IonTitle>User Settings</IonTitle>
+					</IonToolbar>
+				</IonHeader>
+				{user && (
+					<IonContent className="ion-padding">
+						<IonText className="center-text">
+							<h2>Welcome, {userData?.displayName}!</h2>
+						</IonText>
+						<IonImg src={user.photoURL} alt="User  Avatar" className="user-avatar" />
+						<IonButton onClick={handleLogout}>Logout</IonButton>{" "}
+						{/*TODO figure out how to store image, maybe an default user avatar */}
+					</IonContent>
+				)}
+			</IonMenu>
+
+			<IonPage id="main-content">
+				<IonHeader>
+					<IonToolbar>
+						<IonButtons slot="end">
+							{user ? (
+								<IonMenuToggle>
+									<IonImg
+										src={user.photoURL}
+										alt="User  Profile"
+										className="menu-avatar"
+										style={{ cursor: "pointer" }}
+									/>
+								</IonMenuToggle>
+							) : null}
+						</IonButtons>
+						<IonTitle>Landing Page</IonTitle>
+					</IonToolbar>
+				</IonHeader>
+				<IonContent>
+					{!user ? (
+						<div className="login-container">
+							<h2>{isRegistering ? "Register" : "Sign In"}</h2>
+							{errorMessage && <p className="error-message">{errorMessage}</p>}
+							{isRegistering && (
+								<>
+									<IonItem>
+										<IonLabel position="floating">First Name</IonLabel>
+										<IonInput
+											value={firstName}
+											onIonChange={(e) => setFirstName(e.detail.value!)}
+											required
+										/>
+									</IonItem>
+									<IonItem>
+										<IonLabel position="floating">Last Name</IonLabel>
+										<IonInput
+											value={lastName}
+											onIonChange={(e) => setLastName(e.detail.value!)}
+											required
+										/>
+									</IonItem>
+								</>
+							)}
+							<IonItem>
+								<IonLabel position="floating">Email</IonLabel>
+								<IonInput
+									type="email"
+									value={email}
+									onIonChange={(e) => setEmail(e.detail.value!)}
+									required
+								/>
+							</IonItem>
+							<IonItem>
+								<IonLabel position="floating">Password</IonLabel>
+								<IonInput
+									type="password"
+									value={password}
+									onIonChange={(e) => setPassword(e.detail.value!)}
+									required
+								/>
+							</IonItem>
+							<IonButton
+								expand="full"
+								onClick={isRegistering ? handleRegister : handleSignIn}
+							>
+								{isRegistering ? "Register" : "Sign In"}
+							</IonButton>
+							<IonButton
+								expand="full"
+								fill="clear"
+								onClick={() => setIsRegistering(!isRegistering)}
+							>
+								{isRegistering
+									? "Already have an account? Sign In"
+									: "Don't have an account? Register"}
+							</IonButton>
+						</div>
+					) : (
+						<Container userID={user.uid} />
+					)}
+				</IonContent>
+			</IonPage>
+		</React.Fragment>
+	);
+};
 
 export default LandingPage;
